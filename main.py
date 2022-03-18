232498a04e73131ceee715d562978de83e4fdcbb
Asuna <95048094+AsunaAbi@users.noreply.github.com>
2022-03-18 14:46:02 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ class NitroGen:
         chars = []
         chars[:0] = string.ascii_letters + string.digits
 
-        c = numpy.random.choice(chars, size=[num, 23])
+        c = numpy.random.choice(chars, size=[num, 16])
         for s in c:
             try:
                 code = ''.join(x for x in s)
